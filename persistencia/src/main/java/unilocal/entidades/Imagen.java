159a89d15e36efa24c8322718a47b585a6d9440f
Tatianaarboleda <64757270+Tatianaarboleda@users.noreply.github.com>
2021-05-02 23:29:32 -0500

    Se ponen algunos comentarios
    Modificar desde gets y sets

--- persistencia/src/main/java/unilocal/entidades/Imagen.java
+++ persistencia/src/main/java/unilocal/entidades/Imagen.java
@@ -6,46 +6,81 @@ import java.io.Serializable;
 @Entity
 public class Imagen implements Serializable {
 
+    //Número de identificación de una imagen
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
+    //Dirección de la url a la que pertenece la imagen
     @Column(name = "url", nullable = false, unique = true)
     private String url;
 
+    //Lugar el cual la imagen está describiendo
     @ManyToOne
     private Lugar lugar;
 
+     /**
+     * Constructor vacio del horario
+     */
     public Imagen (){
         super();
     }
-
+    
+     /**
+     * Constructor completo para crear una imagen
+     * @param url, url de donde sale la imagen
+     * @param lugar, lugar al que la imagen pertenece
+     */
     public Imagen(String url, Lugar lugar) {
         this.url = url;
         this.lugar = lugar;
     }
 
+    /**
+     * Método que obtiene el numero de identificacion de la imagen
+     * @return el numero de identificación
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Método que modifica el código de identificación del horario
+     * @param codigo, número nuevo de identificación
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Método que obtiene el código de identificación del horario
+     * @return el código de identificación
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Método que modifica el código de identificación del horario
+     * @param codigo, número nuevo de identificación
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * Método que obtiene el código de identificación del horario
+     * @return el código de identificación
+     */
     public Lugar getLugar() {
         return lugar;
     }
 
+    /**
+     * Método que modifica el código de identificación del horario
+     * @param codigo, número nuevo de identificación
+     */
     public void setLugar(Lugar lugar) {
         this.lugar = lugar;
     }
